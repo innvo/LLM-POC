07aa91fd800ecfc6050acb848d0e0e381f082b45
echasin <echasin@innvosolutions.com>
2023-11-25 22:04:01 -0800

    WIP Chunking issues

--- QnA/QnA_bot_v3.py
+++ QnA/QnA_bot_v3.py
@@ -2,10 +2,17 @@
 # Python program that reads files from a folder, creates embeddings using OpenAI, stores them in a Chromata vectorstore, 
 # and responds to user questions using OpenAI LLM with prompt engineering to ensure correct answers:
 import os
+import re
+from langchain import OpenAI
 from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import UnstructuredWordDocumentLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import PGEmbedding
+import openai
+import psycopg2
 from PyPDF2 import PdfReader
 from docx import Document
+import pdfminer
 import spacy
 
 
@@ -13,13 +20,14 @@ import spacy
 os.system('cls' if os.name == 'nt' else 'clear')
 
 folder_path = "QnA/country_reports/content"
-chunk_size = 500
+chunk_size=500
 overlap = 50
+OPENAI_API_KEY='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'
 
 files = os.listdir(folder_path)
 
+## Process pdf document
 def process_pdf_document(full_path):
- def process_pdf_document(full_path):
     print("process_pdf_document: " + full_path)
     pdf = PdfReader(open(full_path, "rb"))
     text = ""
@@ -27,7 +35,14 @@ def process_pdf_document(full_path):
         text += page.extract_text() + "\n"
     with open(full_path.replace('.pdf', '.txt'), 'w') as txt_file:
         txt_file.write(text)
+    # Create chunks
+    chunks = chunk_text(text, chunk_size, overlap)
+    print("Number of records in chunks A: " + str(len(chunks)))
+    embedding_vectors = create_embeddings_gpt3(chunks)
+    write_to_pg_vectorstore(embedding_vectors)
+    #print("Number of chunks is list: ",chunks[0])
 
+## Process docx document
 def process_docx_document(full_path):
     print("process_docx_document: " + full_path)
     doc = Document(full_path)
@@ -36,25 +51,79 @@ def process_docx_document(full_path):
         text += para.text + "\n"
     with open(full_path.replace('.docx', '.txt'), 'w') as txt_file:
         txt_file.write(text)
-    # create chunks
+    # Create chunks
     chunks = chunk_text(text, chunk_size, overlap)
+    # print the number of chunks
+    print("Number of records in chunks A: " + str(len(chunks)))
+ 
+   
 
 ## Chunk text
-def chunk_text(text, chunk_size=500, overlap=50):
+def chunk_text(text, chunk_size, overlap):
     chunks = []
     current_chunk = []
-
     for i in range(0, len(text), chunk_size - overlap):
         chunk = text[i:i+chunk_size]
-        print(" starting token: " + str(i) + " " + "chunk_text: " + str(chunk))
+        # Cleanse chunk
+        chunk = cleanse_text(chunk)
+        # print(" starting token: " + str(i) + " " + "chunk_text: " + str(chunk))
         current_chunk.append(chunk)
-        # Print current chunk
-        # print("current_chunk: " + str(current_chunk))
+        # print("current_chunk: " + str(i) + " " + str(chunk))
         # Maintain overlap between chunks
         if i + chunk_size + overlap < len(text):
             current_chunk.append(text[i+chunk_size:i+chunk_size+overlap])
-    chunks.append(current_chunk)
+        chunks.append(current_chunk)
+    print("Number of records in chunks AB: " + str(len(chunks)))
     return chunks
+
+## Cleanse text
+def cleanse_text(text):
+    # Remove HTML tags
+    text = re.sub(r'<.*?>', '', text)
+    # Convert all characters to lowercase
+    text = text.lower()
+    # Remove punctuation
+    text = re.sub(r'[^\w\s]', '', text)
+    # Remove extra whitespace
+    text = re.sub(r'\s+', ' ', text)
+    # Strip leading and trailing whitespace
+    text = text.strip()
+    # Replace \n with a space
+    text = text.replace('\n', '')
+    return text
+
+## Create embeddings
+def create_embeddings_gpt3(chunks):
+    embeddings = OpenAIEmbeddings() # Load the Langchain OpenAI client
+    embedding_vectors = []
+    for i, chunk in enumerate(chunks):      
+        if chunk[i] != ' ': # Skip empty chunks.  THIS SHOULD BE FIXED in Chunks
+            print("chunk number: " + str(i) + " chunk string: " +  chunk[i])
+            # Create embeddings
+            vectors = embeddings.embed_query(chunk[i])
+            # print("query_result: " + str(vectors))
+            # Create a tuple of chunk and corresponding embedding vectors
+            chunk_and_vectors = (chunk[i], vectors)
+            # Append the tuple to the embedding_vectors array
+            embedding_vectors.append(chunk_and_vectors)
+    return embedding_vectors
+
+def write_to_pg_vectorstore(embedding_vectors):
+    conn = psycopg2.connect("dbname=postgres user=postgres password=postgres host=tp-dev.cr7ro0ecjzwg.us-east-1.rds.amazonaws.com port=5432")
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    for i, (chunk, vector) in enumerate(embedding_vectors):
+        # Execute a query
+        cur.execute("INSERT INTO QnA_bot_vectorstore (id, chunk, vector) VALUES (%s, %s, %s)", (i, chunk, vector))
+    # Commit changes
+    conn.commit()
+
+
+    # Close cursor and connection
+    cur.close()
+    conn.close()
+
 
 
 for file in files:
