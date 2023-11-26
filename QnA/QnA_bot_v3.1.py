bea131f78bb6cfbd6bc39ad16941018bf53e0d8c
echasin <echasin@innvosolutions.com>
2023-11-25 23:37:05 -0800

    WIP moved to text splitter an fixed  pdf to text conversion

--- QnA/QnA_bot_v3.1.py
+++ QnA/QnA_bot_v3.1.py
@@ -21,48 +21,35 @@ import spacy
 os.system('cls' if os.name == 'nt' else 'clear')
 
 folder_path = "QnA/country_reports/content"
-# chunk_size=500
-# overlap = 50
+chunk_size=500
+overlap = 10
 OPENAI_API_KEY='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'
 text_splitter = RecursiveCharacterTextSplitter(
     # Set a really small chunk size, just to show.
-    chunk_size = 100,
-    chunk_overlap  = 20,
-    length_function = len,
-    add_start_index = False,
+    chunk_size = 500,
+    chunk_overlap  = 50
 )
 
 files = os.listdir(folder_path)
 
+## Convert pdf to text
 def convert_pdf_to_text(full_path):
     with open(full_path, 'rb') as f:
         output = pdfminer.high_level.extract_text(f)
     return output
 
-
-
 ## Process pdf document
 def process_pdf_document(full_path):
     print("process_pdf_document: " + full_path)
-    # pdf = PdfReader(open(full_path, "rb")) 
-    # cleanpdf =cleanse_pdf(pdf)
-    # print(cleanpdf)
     text = convert_pdf_to_text(full_path)
-    print(cleanse_text(text))
-
-    # text = ""
-    # for page in pdf.pages:
-    #     text += page.extract_text() + "\n"
-    # with open(full_path.replace('.pdf', '.txt'), 'w') as txt_file:
-    #     txt_file.write(text)
-    #print("text: " + text)
+    # Cleanse text
+    text = cleanse_text(text)
     # Create chunks
     chunks = chunk_text(text)
-    # chunks = chunk_text(text, chunk_size, overlap)
-    #print("Number of records in chunks A: " + str(len(chunks)))
+    # Create embeddings
     embedding_vectors = create_embeddings_gpt3(chunks)
+    # Write to PG vectorstore
     write_to_pg_vectorstore(embedding_vectors)
-    #print("Number of chunks is list: ",chunks[0])
 
 ## Process docx document
 def process_docx_document(full_path):
@@ -77,24 +64,16 @@ def process_docx_document(full_path):
     chunks = chunk_text(text, chunk_size, overlap)
     # print the number of chunks
     print("Number of records in chunks A: " + str(len(chunks)))
- 
-   
+
 
 ## Chunk text
 def chunk_text(text):
     chunks = []
-    #print(text)
-    texts = text_splitter.create_documents(text)
+    texts = text_splitter.create_documents([text])
+    print("Number of records in texts: " + str(len(texts)))
     for i, text in enumerate(texts):
-        # Cleanse chunk
-        # print(" chunk number: " + str(i) + " " + "chunk_text: " + str(text))
-        chunks.append(text)
-        # print("current_chunk: " + str(i) + " " + str(text))
-        # Maintain overlap between chunks
-        # if i + chunk_size + overlap < len(text):
-        #     current_chunk.append(text[i+chunk_size:i+chunk_size+overlap])
-        # chunks.append(current_chunk)
-        #print("texts: " + texts)
+        print("chunk number: " + str(i) + " chunk string: " +  str(texts[i].page_content))
+        chunks.append(texts[i].page_content)
     return chunks
 
 # def chunk_text(text, chunk_size, overlap):
@@ -155,7 +134,7 @@ def create_embeddings_gpt3(chunks):
     embedding_vectors = []
     for i, chunk in enumerate(chunks):      
         if chunk[i] != ' ': # Skip empty chunks.  THIS SHOULD BE FIXED in Chunks
-            print("chunk number: " + str(i) + " chunk string: " +  chunk[i])
+            # print("chunk number: " + str(i) + " chunk string: " +  chunk[i])
             # Create embeddings
             vectors = embeddings.embed_query(chunk[i])
             # print("query_result: " + str(vectors))
@@ -192,8 +171,8 @@ for file in files:
         # pages = loader.load_and_split()
         # do something with pages
         # print(document)
-    if file.endswith('.docx'):
-        full_path = os.path.join(folder_path, file)
+    if file.endswith('.docx'):        full_path = os.path.join(folder_path, file)
         # print(" In docx: " + full_path)
-        process_docx_document(full_path)
+        # process_docx_document(full_path)
+    
       
