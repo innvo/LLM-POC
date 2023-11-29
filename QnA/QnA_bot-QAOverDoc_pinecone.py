437de47613097a36179c636ca96bc3eeed0e6e11
echasin <echasin@innvosolutions.com>
2023-11-28 20:15:36 -0800

    WIP

--- QnA/QnA_bot-QAOverDoc_pinecone.py
+++ QnA/QnA_bot-QAOverDoc_pinecone.py
@@ -25,46 +25,44 @@ index_name = "llm-demo"
 index = pinecone.Index(index_name=index_name)
 
 # Langchain setup
-model = langchain.OpenAI(temperature=0, model_name="gpt-3.5-turbo")
+model = langchain.OpenAI(temperature=0, model_name="gpt-4")
 
 
 def get_documents(response):
-
+    ## Create lists
     ids = []
     scores = [] 
     contents = []
     docs= []
     
+    ## Create docs list for langchain Qa Chain
     for match in response['matches']:
-        #ids.append(match['metadata']['embedding_id'])
-        #scores.append(match['score'])
-        #contents.append(match['metadata']['embedding_content'])
+        ids.append(match['metadata']['embedding_id'])
+        scores.append(match['score'])
+        contents.append(match['metadata']['embedding_content'])
         content=match['metadata']['embedding_content']
   
-         # Create Document object
+        # Create Document object
         doc = Document(
             page_content=content
         )
         docs.append(doc)
 
-
-
-    df = pd.DataFrame({
+    ## Create a dataframe
+    search_results_df = pd.DataFrame({
         #'id': ids,
         #'score': scores,
         'page_content': contents
     })
 
-    #docs = df.to_dict(orient='records') 
-    print(docs)
-
+    # Load QA Chain
     qa_chain = load_qa_chain(model, chain_type="stuff")
     response = qa_chain.run(
         question=question, 
         input_documents=docs
     )  
     print(response)
-    return df
+    return docs
 
 # Generate the query embedding
 def answer_question(question):
@@ -72,28 +70,14 @@ def answer_question(question):
     question_emb = embeddings.embed_query(question)
 
     # Perform the query
-    response = index.query([question_emb], top_k=3, include_metadata=True, include_values=False)
+    response = index.query([question_emb], top_k=10, include_metadata=True, include_values=False)
 
     get_documents(response)
   
+#question =  "What did the president say about Justice Breyer" 
+#question =  "What did the president say about Ukraine"
+# question = "What are the top 5 topics discussed"
+question = "What is the president' birthday"
 
-    
-    # Get response for each prompt
-        # X = res['matches'][1]['metadata']['embedding_content']
-        # print(X)
-        # prompt = f"Human: {question}\nAssistant: Based on the following context: {context}\n"
-
-    # # response = model.generate(prompt)
-    # print(prompt)
-
-    #  # Get response for each prompt
-    # responses = model.generate(
-    #     [prompt]
-    # )
-
-    # Return response list
-   #  return responses
-
-question =  "What did the president say about Justice Breyer" 
 answer = answer_question(question)
-#print(answer)
+
--- QnA/QnA_bot_semantic_search_pinecone.py
+++ QnA/QnA_bot_semantic_search_pinecone.py
@@ -8,9 +8,10 @@ import json
 import pinecone
 import os
 
-# Clear the terminal
+#3#Clear the terminal
 os.system('cls' if os.name == 'nt' else 'clear')
 
+## Set local environment variables
 embeddings = OpenAIEmbeddings()
 pinecone.init(api_key="ef9a9434-5233-4b29-a794-355b106be8d7",
               environment="us-west4-gcp-free")
@@ -19,39 +20,15 @@ index_name = "llm-demo"
 # Create a Pinecone index object
 index = pinecone.Index(index_name=index_name)
 
-## No Match
+## Question
 query_string = "What did the president say about Justice Breyer" 
-## Exact Match
-# query_string ="abductions and physical abuse by nonstate armed groups serious restrictions on freedom of expression including violence threats of violence or unjustified detentions of journalists and censorship substantial interference with the right of peaceful assembly and freedom of association serious restrictions on freedom of movement inability of citizens to change their government peacefully through free and fair elections serious and unreasonable restrictions on political participation serious"
-# Partial Match
-# query_string ="what is the ruling political party in Cameroon"
-# query_string ="Who is Paul Biya"
-
-# Generate the query embedding
+## Generate the query embedding
 query_embedding = embeddings.embed_query(query_string)
 
-# Perform the query
+## Perform the query
 search_results = index.query(query_embedding, top_k=3, include_metadata=True, include_values=False)
 
-# Print the search results
+## Print the search results
 print("Search results:", search_results)
-# for result in search_results:
-#     print(result.id, result.score, result.value)
 
-
-
-
-# # Query the index
-# results = index.query(
-#     vector=[query_embedding],
-#     top_k= 1,
-#     include_values=True
-#     )
-
-# # Print the results
-# for result in results.results:
-#     print(f"ID: {result.id}, Score: {result.score}")
-
-# Deinitialize Pinecone
-# pinecone.deinit()
 
