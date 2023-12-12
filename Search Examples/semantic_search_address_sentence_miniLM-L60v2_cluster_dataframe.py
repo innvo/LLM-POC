d21032aeaaa689f0c2ab6cb44c71eb16a03ca0b9
echasin <echasin@innvosolutions.com>
2023-12-12 10:12:05 -0800

    added pandas clustering with dataframe

---
+++ Search Examples/semantic_search_address_sentence_miniLM-L60v2_cluster_dataframe.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer  
+from sklearn.metrics.pairwise import cosine_distances
+from sklearn.cluster import DBSCAN
+
+# Create model and encode addresses
+model = SentenceTransformer('all-MiniLM-L6-v2')
+addresses = [
+    '123 Apple St UNIT 1 New York NY',
+    '123 Maple Street New York New York 22012',
+    '123 Apple Street Unit 1 NY, NY',    
+    '123 Maple St New York NY',
+    '452 Main St New York NY',  
+]
+vectors = model.encode(addresses)
+
+# Calculate distances and similarities  
+distance_matrix = cosine_distances(vectors)
+similarity_matrix = 1 - distance_matrix
+
+# Cluster addresses 
+clustering = DBSCAN(metric='precomputed', eps=0.2, min_samples=2).fit(distance_matrix)
+cluster_labels = clustering.labels_
+
+# Create dataframes  
+cluster_df = pd.DataFrame({'Address': addresses, 'Cluster': cluster_labels})
+similarity_df = pd.DataFrame(similarity_matrix, columns=addresses, index=addresses)
+
+# Print outputs 
+print(cluster_df)
+print() 
+print(similarity_df.round(3))
